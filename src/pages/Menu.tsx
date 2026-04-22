76ca8a72a667540454950e0941c8ce0f6a7847f5
Jabezbinayao <jabez.binayao@gmail.com>
2026-04-22 15:55:15 +0800

    add list

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,60 +1,61 @@
-import { IonButton, IonIcon, IonContent, IonHeader, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar } from "@ionic/react";
-import { Redirect, Route } from "react-router";
-import { homeOutline, logOutOutline } from "ionicons/icons";
-import Home from "./Home";
+import React from 'react';
+import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonSplitPane, IonMenu, IonRouterOutlet, IonItem, IonIcon, IonMenuToggle, IonButton } from '@ionic/react';
+import { homeOutline, logOutOutline } from 'ionicons/icons';
+import { Route, Redirect } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
-const Menu: React.FC = () => {
+const Menu: React.FC = () =>{
 
-    const path = [
-        {name : 'Home' , url:'/app/home', icon:homeOutline}
-    ]
-    
-      
-    
-   return(
+  const path = [
+    {name: 'Home', url: '/app/home', icon: homeOutline},
+     {name: 'About', url: '/app/home', icon: homeOutline}
+  ]
+
+  return(
     <IonPage>
-       <IonSplitPane contentId="main">
+      <IonSplitPane contentId="main">
+
         <IonMenu contentId="main">
+          <IonHeader>
+            <IonToolbar> 
+              <IonTitle>
+                Menu
+              </IonTitle>
+            </IonToolbar>                    
+          </IonHeader>
 
-            <IonHeader>               
-                <IonToolbar>
-                    <IonTitle>
-                        Menu
-                    </IonTitle>
-                </IonToolbar>
-            </IonHeader>
+          <IonContent>
+            {path.map((item, index) => (
+              <IonMenuToggle key={index}>
+                <IonItem routerLink={item.url} routerDirection="forward">
+                  <IonIcon icon={item.icon} slot="start"></IonIcon>
+                  {item.name}
+                </IonItem>
+              </IonMenuToggle>
+            ))}
 
-            <IonContent>
+            <IonButton routerLink="/" routerDirection="back" expand="full">
+              <IonIcon icon={logOutOutline} slot="start"></IonIcon>
+              Logout
+            </IonButton>
 
-                {path.map((item,index) => (
-                    <IonMenuToggle key={index}>
-                        <IonItem routerLink={item.url} routerDirection="forward">
-                            <IonIcon icon={item.icon} slot="start">
-                                {item.name}
-                            </IonIcon>
-                        </IonItem>
-                    </IonMenuToggle>
-                ))}
-                <IonButton routerLink="/" routerDirection="back" expand= "full">
-                <IonIcon icon={logOutOutline} slot= "start" ></IonIcon>
-                Logout
-                </IonButton>
-
-            </IonContent>
+          </IonContent>
         </IonMenu>
 
         <IonRouterOutlet id="main">
-            <Route exact path="/app/Home" component ={Home}/>
-            <Route exact path="/app">
-                 <Redirect to="/app/Home" />
-            </Route>
+          <Route exact path="/app/home" component={Home} />
+          <Route exact path="/app/about" component={About} />
 
+          <Route exact path="/app">
+            <Redirect to="/app/home" />
+          </Route>
         </IonRouterOutlet>
 
-       </IonSplitPane>
+      </IonSplitPane>
     </IonPage>
-
-   );
+  );
 
 };
+
 export default Menu;
--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,21 +1,22 @@
 
 import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from '@ionic/react';
 
-const Template: React.FC = () =>{
+const Search: React.FC = () =>{
       return(
            <IonPage>
               <IonHeader>
                   <IonToolbar>
                     <IonButtons>
                     <IonMenuButton></IonMenuButton>
+                     <IonTitle>Search</IonTitle>
                   </IonButtons>
-                  <IonTitle>Template</IonTitle>
                 </IonToolbar>
               </IonHeader>
-              <IonContent fullscreen>
+               <IonContent className="ion-padding">
+                <h1>Search</h1>
               </IonContent>
               </IonPage>
       );
 
 };
-export default Template;
+export default Search;
